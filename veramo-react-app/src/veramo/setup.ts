042ebc404fcac97b4eb8af4e300c7cf13acd0974
jk2500 <be21b029@smail.iitm.ac.in>
2023-03-04 15:30:29 +0000

    Removed CredentialLD Plugin

--- veramo-react-app/src/veramo/setup.ts
+++ veramo-react-app/src/veramo/setup.ts
@@ -1,6 +1,5 @@
 import { createAgent, IResolver } from '@veramo/core'
 import {CredentialPlugin, ICredentialVerifier} from '@veramo/credential-w3c'
-import {ICredentialIssuerLD} from '@veramo/credential-ld'
 import { DIDResolverPlugin } from '@veramo/did-resolver'
 import { Resolver } from 'did-resolver'
 import { getResolver as ethrDidResolver, verificationMethodTypes } from 'ethr-did-resolver'
@@ -9,7 +8,7 @@ import { getResolver as webDidResolver } from 'web-did-resolver'
 // You will need to get a project ID from infura https://www.infura.io
 const INFURA_PROJECT_ID = '33aab9e0334c44b0a2e0c57c15302608'
 
-export const agent = createAgent<IResolver & ICredentialVerifier & ICredentialIssuerLD>({
+export const agent = createAgent<IResolver & ICredentialVerifier>({
   plugins: [
     new DIDResolverPlugin({
       resolver: new Resolver({
